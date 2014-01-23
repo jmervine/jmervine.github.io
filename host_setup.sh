f2453be41cd02208e70268edf82624fbe694f66f
Joshua Mervine <joshua@mervine.net>
2014-01-23 11:20:36 -0800

    Update host_setup.sh

--- host_setup.sh
+++ host_setup.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 #
 # curl -sSL https://github.com/jmervine/me/edit/master/host_setup.sh | bash # -s centos
+#
+# The resulting configuration has been verified on:
+#
+# * VIM 7.2.411 and some later versions.
+# * ZSH 4.3.10
+# * TMUX 1.6
+#
 
 ALTERNATE=false
 test "$1" && ALTERNATE=true
